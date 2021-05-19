aaf558849103bf57c0d21c77aad220a2e65b22ac
Adrien Fauquembergue <adrien_fauque@hotmail.fr>
2021-05-19 16:31:29 +0200

    modified

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_05_19_100243) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,12 +35,9 @@ ActiveRecord::Schema.define(version: 2021_05_19_100243) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "time_flight"
-
-    t.integer "total_bookings", default: 0
-
     t.float "latitude"
     t.float "longitude"
-
+    t.integer "total_bookings", default: 0
     t.index ["user_id"], name: "index_travels_on_user_id"
   end
 
